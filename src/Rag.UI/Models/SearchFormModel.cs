6a384ffda8610cac9d10df74a0513744e75d85c9
Mike Eckel <mike.eckel@education.ne.gov>
2025-11-17 13:45:32 -0600

    update: search validation added

---
+++ src/Rag.UI/Models/SearchFormModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rag.UI.Models;
+
+public class SearchFormModel
+{
+    [Required(ErrorMessage = "Please enter a search query")]
+    public string Query { get; set; } = string.Empty;
+}
