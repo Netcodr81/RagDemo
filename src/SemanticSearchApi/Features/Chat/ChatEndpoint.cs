6ef776e8c9f0722a3c2ef310110f54b32f794584
Mike Eckel <mike.eckel@education.ne.gov>
2025-11-18 15:22:55 -0600

    EOD Commit - Chat page started

---
+++ src/SemanticSearchApi/Features/Chat/ChatEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.AI;
+using SemanticSearchApi.Services;
+
+namespace SemanticSearchApi.Features.Chat;
+
+public class ChatEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/chat", async Task<IResult> (
+            [FromBody] List<ChatMessage> messages,
+            [FromServices] IChatClient chatClient,
+            [FromServices] ChatOptions chatOptions,
+            [FromServices] PromptService promptService
+        ) =>
+        {
+            var withSystemPrompt = (new[] {new ChatMessage(ChatRole.System, promptService.ChatSystemPrompt)})
+                .Concat(messages)
+                .ToList();
+            
+            var response = await chatClient.GetResponseAsync(withSystemPrompt, chatOptions);
+            
+            return Results.Ok(response.Messages);
+        }).Produces<IEnumerable<ChatMessage>>();
+    }
+}
